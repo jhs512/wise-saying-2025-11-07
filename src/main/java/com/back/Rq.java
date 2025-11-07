23ff7099db7d17e2a42c799476c9591787158284
jhs512 <jangka512@gmail.com>
2025-11-07 14:31:37 +0900

    Rq 완성

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -1,18 +1,43 @@
 package com.back;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Rq {
     private final String actionName;
+    private final Map<String, String> params;
 
     public Rq(String cmd) {
-        actionName = cmd;
+        String[] cmdBits = cmd.split("\\?", 2);
+
+        actionName = cmdBits[0];
+        String queryString = cmdBits.length > 1 ? cmdBits[1] : "";
+
+        params = Arrays
+                .stream(queryString.split("&"))
+                .map(queryParam -> queryParam.split("=", 2))
+                .filter(e -> e.length == 2 && !e[0].isBlank() && !e[1].isBlank())
+                .collect(Collectors.toMap(
+                        bits -> bits[0].trim(),
+                        bits -> bits[1].trim()
+                ));
     }
 
     public String getParam(String name, String defaultValue) {
-        return defaultValue;
+        return params.getOrDefault(name, defaultValue);
     }
 
     public int getParamAsInt(String name, int defaultValue) {
-        return defaultValue;
+        String value = getParam(name, "");
+
+        if (value.isBlank()) return defaultValue;
+
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 
     public String getActionName() {
